074d9860ec6c8448acd3d17bc6f89a87c6040c78
Evgeniy Schemelinin <Kovarniy@users.noreply.github.com>
2020-04-17 01:18:40 +0800

    Simple class Graphs witch dfs

--- graph-algorithms/graph-algorithms.cpp
+++ graph-algorithms/graph-algorithms.cpp
@@ -6,24 +6,13 @@
 using namespace std;
 
 string inputPuth = "input.txt";
-vector<bool> lable;
 
 class Graph {
 private:
 	vector<vector<int>> adjMatrix;
 	vector<vector<int>> adjList;
+	vector<bool> label;
 	int _matrSize;
-	ifstream fin;
-
-public:
-	Graph(int matrSize) {
-		_matrSize = matrSize;
-
-		fin.open(inputPuth);
-		for (int i = 0; i < _matrSize; i++)
-			for (int j = 0; j < _matrSize; j++)
-				fin >> adjMatrix[i][j];
-	}
 
 	void createAdjList() {
 		adjList.resize(_matrSize, vector<int>());
@@ -38,63 +27,66 @@ public:
 		}
 	}
 
+	void lableInit() {
+		label.resize(_matrSize, false);
+	}
+	// This function returnes label to false state
+	void lableReload() {
+		for (int i = 0; i < label.size(); i++)
+			label[i] = false;
+	}
+
+public:
+	// TODO: сделать возможность ввода данных из текстового файла и с клавиатуры
+	Graph() {
+		ifstream fin;
+		fin.open(inputPuth);
+		fin >> _matrSize;
+
+		adjMatrix.resize(_matrSize, vector<int>(_matrSize));
+
+		for (int i = 0; i < _matrSize; i++)
+			for (int j = 0; j < _matrSize; j++)
+				fin >> adjMatrix[i][j];
+
+		createAdjList();
+		lableInit();
+	}
+
+	~Graph() {
+		cout << "Goodbye!" << endl;
+		// Distructure for vector. Is it necessary? 
+		//delete[] label;
+	}
+
+	void printMatrix() {
+		for (int i = 0; i < _matrSize; i++, cout << endl)
+			for (int j = 0; j < _matrSize; j++)
+				cout << adjMatrix[i][j];
+	}
+
+	void printAdjList() {
+		for (int i = 0; i < _matrSize; i++, cout << endl)
+			for (int j = 0; j < adjList[i].size(); j++)
+				cout << adjList[i][j];
+	}
+
+	// this function print dfs trace
+	void dfs(int vertexNum) {
+		cout << vertexNum << " ";
+		label[vertexNum] = true;
+		for (auto iter : adjList[vertexNum])
+			if (label[iter] == false) dfs(iter);
+	}
 
 };
 
-
-// TODO: продумать структуру фунуций. Нужно ли передвать размер? т.к. мы работает с вектором, а не массивом
-// Нужно ли делать шаблонные функции? 
-void printMatrix(int matrSize, vector<vector<int>> matrix) {
-	for (int i = 0; i < matrSize; i++, cout << endl)
-		for (int j = 0; j < matrix[i].size(); j++)
-			cout << matrix[i][j];
-}
-
-vector<vector<int>> createAdjList(int matrSize, vector<vector<int>> matrix) {
-	vector<vector<int>> adjList(matrSize, vector<int>());
-	for (int i = 0; i < matrSize; i++) {
-		// j = i + 1; - небольшая оптимизация, для того чтобы обохоидть только верхнюю часть матрицы
-		for (int j = i + 1; j < matrSize; j++) {
-			if (matrix[i][j] > 0)
-			{
-				adjList[i].push_back(j);
-				adjList[j].push_back(i);
-			}
-		}
-	}
-	return adjList;
-}
-
-// adjList - список смежности
-void dfs(int vertexNum, vector<vector<int>> adjList) {
-	cout << vertexNum << " ";
-	lable[vertexNum] = true;
-	for (auto iter : adjList[vertexNum])
-		if (lable[iter] == false) dfs(iter, adjList);
-
-}
-
 int main()
 {
-	/*
-		ifstream fin;
-		fin.open(inputPuth);
-		int matrSize;
-
-		fin >> matrSize;
-		vector<vector<int>> G(matrSize, vector<int>(matrSize));
-		lable.resize(matrSize, false);
-
-		for (int i = 0; i < matrSize; i++)
-			for (int j = 0; j < matrSize; j++)
-				fin >> G[i][j];
-
-		printMatrix(matrSize, G);
-		vector<vector<int>> adjList = createAdjList(matrSize, G);
-		printMatrix(matrSize, adjList);
-		dfs(0, adjList);
-	*/
-
-	Graph g(1);
-
+	Graph g1;
+	g1.printMatrix();
+	g1.printAdjList();
+	g1.dfs(1); 
+	vector<bool> v;
+	v.resize(4, false);
 }
